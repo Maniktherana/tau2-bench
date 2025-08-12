850ee9b83477b34ea651ae7601b0d452b4890282
Manik <Manikrana54@gmail.com>
2025-08-12 15:02:59 +0000

    feat: add a data model

---
+++ src/tau2/domains/banking/utilts.py
@@ -0,0 +1,6 @@
+from tau2.utils.utils import DATA_DIR
+
+BANKING_DATA_DIR = DATA_DIR / "tau2" / "domains" / "banking"
+BANKING_DB_PATH = BANKING_DATA_DIR / "db.toml"
+BANKING_POLICY_PATH = BANKING_DATA_DIR / "policy.md"
+BANKING_USER_POLICY_PATH = BANKING_DATA_DIR / "user_policy.md"
